87959c466191a6d8660a1584409b90533a011373
steve111mv <unlivregratuit@gmail.com>
2020-10-15 23:35:46 +0100

    Fix: correcting the intent bug by adding the missing Uri

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -31,7 +31,7 @@ public class InfoActivity extends AppCompatActivity {
     public void createMapIntent(View view){
 
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
         mapIntent.setPackage("com.google.android.apps.maps");
 
         if(mapIntent.resolveActivity(getPackageManager()) != null){
